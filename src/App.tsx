e309d5234b52907f7fd3c54d148319253a1c03ef
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-10-02 18:13:01 -0400

    feat: Initialize Vite project with React and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SideNavBar from './components/SideNavBar';
+
+function App() {
+    return (
+        <section className='p-8'>
+            <SideNavBar />
+        </section>
+    );
+}
+
+export default App;
