b7f3216126cd1599b3f92a88d2c21eb68c1cd999
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-10-03 22:58:49 -0400

    create routing to whit react-router and react-router-dom

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import AppLayout from '../layout/AppLayout';
+import Error_404 from '../pages/Error_404';
+import Pomodoro from '../components/Pomodoro';
+
+export const router = createBrowserRouter([
+    {
+        path: '*',
+        element: <Error_404 />,
+    },
+
+    {
+        path: '/',
+        element: <AppLayout />,
+    },
+
+    {
+        path: '/tasks',
+        element: <AppLayout title="Explore tasks" />,
+    },
+
+    {
+        path: '/pomodoro',
+        element: <AppLayout title="Pomodoro" />,
+        children: [{ path: '', element: <Pomodoro className="size-full lg:hidden" /> }],
+    },
+
+    {
+        path: '/groups',
+        element: <AppLayout title="Explore groups" />,
+    },
+
+    {
+        path: '/settings',
+        element: <AppLayout title="Settings" />,
+    },
+]);
