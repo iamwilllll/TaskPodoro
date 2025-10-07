81b5820a759b5be9947d2282b0bc3d6ac8d7d210
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-10-07 07:50:32 -0400

    refactor(overview): Remove hardcoded group items

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -7,9 +7,6 @@ function Overview() {
     return (
         <section className="grid grid-cols-1 grid-rows-3 place-items-center gap-5 md:grid-cols-3 lg:grid-cols-4">
             <AddGroupItem addGroup={addGroup} />
-            <AddGroupItem addGroup={addGroup} />
-            <AddGroupItem addGroup={addGroup} />
-            <AddGroupItem addGroup={addGroup} />
         </section>
     );
 }
