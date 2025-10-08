1588419729064759200a0b6f57a22070d79926e7
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-10-08 16:37:02 -0400

    feat: Display and add groups on Overview page

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,12 +1,28 @@
 import AddGroupItem from '../components/AddGroupItem';
+import type { GroupsTDraftT } from '../types';
 import { useGroup } from '../hooks/useGroup';
+import { useGroupContext } from '../context/store';
+import GroupItem from '../components/GroupItem';
 
 function Overview() {
-    const { addGroup } = useGroup();
+    const { createGroups } = useGroup();
+    const { GROUPS, MAX_GROUPS } = useGroupContext();
+
+    const group: GroupsTDraftT = {
+        name: 'Homework',
+        description:
+            'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+        image: 'https://i.pinimg.com/474x/07/c4/72/07c4720d19a9e9edad9d0e939eca304a.jpg',
+        tasks: [],
+    };
 
     return (
         <section className="grid grid-cols-1 grid-rows-3 place-items-center gap-5 md:grid-cols-3 lg:grid-cols-4">
-            <AddGroupItem addGroup={addGroup} />
+            <AddGroupItem className={`${MAX_GROUPS ? 'hidden' : 'grid'}`} addGroup={() => createGroups(group)} />
+
+            {GROUPS.map((group, index) => {
+                return <GroupItem key={index} group={group} />;
+            })}
         </section>
     );
 }
