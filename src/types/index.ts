3984cebc8aad6530c505cd8aefe3e9dfab53b781
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-10-08 16:37:01 -0400

    feat: Define and expand types for groups and tasks

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,17 @@
-export type GroupsT  = {
-    id
-    name:string
-}
+export type GroupsT = {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    tasks: Tasks[];
+};
 
-export type GroupsTDraft = Omit
+export type GroupsTDraftT = Omit<GroupsT, 'id'>;
+
+export type Tasks = {
+    id: string;
+    name: string;
+    description: string;
+};
+
+export type TasksTDraftT = Omit<Tasks, 'id'>;
