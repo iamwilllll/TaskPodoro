389f9476daed3b17d188c5214d514df104a8bd0e
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-10-09 17:32:56 -0400

    add Groups page route and integrate into AppRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import AppLayout from '../layout/AppLayout';
 import Error_404 from '../pages/Error_404';
 import Pomodoro from '../components/Pomodoro';
 import Overview from '../pages/Overview';
+import Groups from '../pages/Groups';
 
 export const router = createBrowserRouter([
     {
@@ -31,6 +32,7 @@ export const router = createBrowserRouter([
     {
         path: '/groups',
         element: <AppLayout title="Explore groups" />,
+        children: [{ path: '', element: <Groups /> }],
     },
 
     {
